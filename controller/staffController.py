d75a555d16588264d2365717fd93cfcf0822f1c1
Adam <adam.gruber@cpnv.ch>
2022-05-29 18:55:36 +0200

    Suppression des fichiers inutiles

--- controller/staffController.py
+++
@@ -1,9 +0,0 @@
-from PyQt5 import QtWidgets, QtGui, uic
-import sys
-
-class StaffUi(QtWidgets.QMainWindow):
-    def __init__(self):
-        super().__init__()
-        print("In staff")
-        self.StaffWindow = uic.loadUi(sys.path[0] + "\\view\\staffsView.ui", self)
-        self.StaffWindow.show()
